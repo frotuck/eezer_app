625e8f6102174ee3489105ef5b230e523a4565b7
Jenseneducation-Lukas-Tucker <54941820+Jenseneducation-Lukas-Tucker@users.noreply.github.com>
2020-10-26 11:48:24 +0100

    added our primary colours and icon shortcuts in plugins/vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,6 +4,9 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdiSvg', // default - only for display purposes
+      },
     theme: {
         themes: {
           light: {
