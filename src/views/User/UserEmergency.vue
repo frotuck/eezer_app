97b0c6f7bccac9a45441b250f4f555d4d0570990
Artur <Artur.Hoppner@hotmail.com>
2020-11-03 14:30:21 +0100

    fixed 70% of user views mobile

--- src/views/User/UserEmergency.vue
+++ src/views/User/UserEmergency.vue
@@ -19,30 +19,44 @@
         <v-img alt="logout" src="../../assets/logout-icon.png" max-width="35" />
       </router-link>
     </v-app-bar>
-    <v-card height="150">
-      <v-footer
-        color="primaryblack"
-        absolute
-        class="font-weight-medium d-sm-none"
-      >
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <router-link to="/useremergency" class="icon-footer">
-          <v-img src="../../assets/alert-active.png" min-width="30" />
-        </router-link>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <router-link to="/user">
-          <v-img src="../../assets/explore-inactive.png" min-width="30" />
-        </router-link>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <router-link to="/userprofile">
-          <v-img src="../../assets/profile-inactive.png" min-width="30" />
-        </router-link>
-        <v-spacer></v-spacer>
-      </v-footer>
-    </v-card>
+    <h2>What to do</h2>
+    <h4>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut ero labore et dolore magna aliqua. Lorem ipsum dolor
+      sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+      ut ero labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
+      consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero
+      labore et dolore magna aliqua.
+    </h4>
+    <v-img
+      alt="logout"
+      src="../../assets/warning-mobile.png"
+      max-width="80vw"
+    />
+
+    <v-footer
+      min-height="60px"
+      color="primaryblack"
+      absolute
+      class="font-weight-medium d-sm-none"
+    >
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <router-link to="/useremergency" class="icon-footer">
+        <v-img src="../../assets/alert-active.png" min-width="35" />
+      </router-link>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <router-link to="/user">
+        <v-img src="../../assets/explore-inactive.png" min-width="35" />
+      </router-link>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <router-link to="/userprofile">
+        <v-img src="../../assets/profile-inactive.png" min-width="35" />
+      </router-link>
+      <v-spacer></v-spacer>
+    </v-footer>
   </div>
 </template>
 
