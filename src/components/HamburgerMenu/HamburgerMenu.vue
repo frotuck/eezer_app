d895a01ac9eee84cd38a656845c76f683605c217
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-11-03 21:22:19 +0100

    styled hamburgermenu and added it to all driver components (mobile only)

---
+++ src/components/HamburgerMenu/HamburgerMenu.vue
@@ -0,0 +1,125 @@
+<template>
+  <!-- ###################### -->
+  <!-- #### DESKTOP VIEW #### -->
+  <!-- ###################### -->
+
+  <!-- ##################### -->
+  <!-- #### MOBILE VIEW #### -->
+  <!-- ##################### -->
+  <v-container>
+    <v-dialog overlay v-model="showMenu">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on" class="primaryblack button right" dark>
+          <v-icon>mdi-menu</v-icon></v-btn
+        >
+      </template>
+      <v-card
+        min-height="70vh"
+        max-width="80vw"
+        class="d-flex flex-column justify-center align-center primaryblack position"
+      >
+        <v-btn
+          depressed
+          dark
+          width="20vw"
+          class="primaryblack button close"
+          @click="showMenu = !showMenu"
+          ><v-icon>mdi-close</v-icon></v-btn
+        >
+        <v-btn
+          router-link
+          to="/emergency"
+          depressed
+          class="d-flex justify-space-evenly primary size margin"
+          ><v-icon class="absolute-left">mdi-bell</v-icon>Emergency</v-btn
+        >
+        <v-btn
+          router-link
+          to="/profile"
+          depressed
+          class="d-flex justify-space-evenly primary size margin"
+          ><v-icon class="absolute-left">mdi-account</v-icon>Profile</v-btn
+        >
+        <v-btn
+          router-link
+          to="/map"
+          depressed
+          class="d-flex justify-space-evenly primary size margin"
+          ><v-icon class="absolute-left">mdi-compass</v-icon>Map</v-btn
+        >
+        <v-btn
+          router-link
+          to="/statistics"
+          depressed
+          class="d-flex justify-space-evenly primary size margin"
+          ><v-icon class="absolute-left">mdi-chart-bar</v-icon>Statistics</v-btn
+        >
+        <v-btn
+          router-link
+          to="/help"
+          depressed
+          class="d-flex justify-space-evenly primary size margin"
+          ><v-icon class="absolute-left">mdi-help</v-icon>Help</v-btn
+        >
+        <v-btn
+          router-link
+          to="/"
+          depressed
+          class="d-flex justify-space-evenly primary size margin"
+          ><v-icon class="absolute-left">mdi-exit-to-app</v-icon>Logout</v-btn
+        >
+      </v-card>
+    </v-dialog>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'HamburgerMenu',
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.position {
+  position: absolute;
+  top: 0%;
+  right: 0%;
+  // transform: translate(-50%, -50%);
+}
+
+.button {
+  outline: none;
+  border: none;
+  box-shadow: none;
+}
+
+.size {
+  width: 60vw;
+  height: 40vh;
+}
+
+.close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-top: 5%;
+  // margin-left: 60%;
+}
+
+.absolute-left {
+  position: absolute;
+  left: 0;
+}
+
+.margin {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.right {
+  margin-left: 80%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import SignInDialogPersonnel from './components/SignIn/SignInDialogPersonnel.vue'
-import SignInDialogUser from './components/SignIn/SignInDialogUser.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import SignInDialogPersonnel from './components/SignIn/SignInDialogPersonnel.vue';
+import SignInDialogUser from './components/SignIn/SignInDialogUser.vue';
+import HamburgerMenu from './components/HamburgerMenu/HamburgerMenu.vue';
 
-Vue.config.productionTip = false
-Vue.component('app-dialog-user', SignInDialogUser)
-Vue.component('app-dialog-personnel', SignInDialogPersonnel)
-
+Vue.config.productionTip = false;
+Vue.component('app-dialog-user', SignInDialogUser);
+Vue.component('app-dialog-personnel', SignInDialogPersonnel);
+Vue.component('app-hamburger-menu', HamburgerMenu);
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
