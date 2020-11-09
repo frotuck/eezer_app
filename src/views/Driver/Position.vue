7474c35d55f7bb523d64f520278823006b5cf65e
Jenseneducation-Lukas-Tucker <54941820+Jenseneducation-Lukas-Tucker@users.noreply.github.com>
2020-11-09 15:40:44 +0100

    fixed responsiveness

--- src/views/Driver/Position.vue
+++ src/views/Driver/Position.vue
@@ -1,65 +1,81 @@
 <template>
-  <div>
-    <!-- ###################### -->
-    <!-- #### DESKTOP VIEW #### -->
-    <!-- ###################### -->
-    <div class="d-none d-sm-flex">
-      <v-app-bar app color="#202020">
-        <v-img
-          class="d-none d-sm-flex header-icon"
-          alt="Eezerlogo"
-          src="../../assets/eezer-logo.png"
-          max-width="55"
-        />
-        
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-
-        <v-spacer></v-spacer>
-        
-        <router-link to="/position">
+  <div id="desktop-background">
+    <!--   ##################### -->
+    <!--  ###### HEADER ###### -->
+    <!-- ################### -->
+    <v-app-bar app color="primaryblack text-center pa-0">
+      <v-container
+        max-width="60%"
+        height="100%"
+        class="d-flex justify-space-around align-center pa-0 ma-0"
+      >
+        <h2
+          justify="center"
+          class="header-page-name justify-self-center d-md-none"
+        >
+          Position
+        </h2>
+        <v-container
+          max-width="60%"
+          height="100%"
+          class="d-none d-md-flex justify-space-around pa-0 ma-0"
+        >
           <v-img
-            style="margin-bottom: -26px"
-            alt="logout"
-            src="../../assets/map-desktop-active.png"
-            max-width="120px"
+            class="d-flex header-icon"
+            alt="Eezerlogo"
+            style="margin-left: 5%;"
+            src="../../assets/eezer-logo.png"
+            max-width="55"
           />
-        </router-link>
-        <router-link to="/profile">
-          <v-img
-            style="margin-bottom: -26px"
-            alt="logout"
-            src="../../assets/profile-desktop-inactive.png"
-            max-width="120px"
-          />
-        </router-link>
-        <router-link to="/emergency">
-          <v-img
-            style="margin-bottom: -26px"
-            alt="logout"
-            src="../../assets/emergency-desktop-inactive.png"
-            max-width="120px"
-          />
-        </router-link>
-        <v-spacer></v-spacer>
-
-        <app-hamburger-menu style="margin-right: -40px"></app-hamburger-menu>
-      </v-app-bar>
-    </div>
-      <v-card
-    class="mx-auto mt-sm-5 mt-8"
-    max-width="800"
-  >
-        <v-toolbar
+          <v-container
+            height="100%"
+            max-width="80%"
+            class="d-flex justify-end align-end pa-0 margin-right-fix"
+          >
+            <router-link to="/position">
+              <v-img
+                style="margin-bottom: -4px;"
+                alt="logout"
+                src="../../assets/map-desktop-inactive.png"
+                max-width="120px"
+              />
+            </router-link>
+            <router-link to="/profile">
+              <v-img
+                style="margin-bottom: -4px;"
+                alt="logout"
+                src="../../assets/profile-desktop-inactive.png"
+                max-width="120px"
+              />
+            </router-link>
+            <router-link to="/emergency">
+              <v-img
+                style="margin-bottom: -4px;"
+                alt="logout"
+                src="../../assets/emergency-desktop-inactive.png"
+                max-width="120px"
+              />
+            </router-link>
+          </v-container>
+        </v-container>
+      </v-container>
+      <app-hamburger-menu></app-hamburger-menu>
+    </v-app-bar>
+    <!--   ############################ -->
+    <!--  ###### PAGE CONTENT ####### -->
+    <!-- ########################## -->
+    <v-card class="box-shadow-none size">
+      <v-toolbar
         class="primaryblack text-center d-none d-md-flex justify-center"
         dark
       >
         <h3>Route</h3>
       </v-toolbar>
+      <v-container class="pa-0"> 
+      <v-card
+    class="mx-auto mt-sm-5 mt-8"
+    max-width="1000"
+  >
     <v-img
       class="white--text align-end"
       height="500px"
@@ -123,55 +139,73 @@
     </v-card-actions>
     </div>
   </v-card>
-    <v-footer
-      min-height="40px"
-      color="#202020"
-      absolute
-      class="d-none d-sm-flex"
-    >
-    </v-footer>
-    <!-- ##################### -->
-    <!-- #### MOBILE VIEW #### -->
-    <!-- ##################### -->
-    <v-app-bar app class="d-sm-none" color="primaryblack">
-      <v-spacer></v-spacer>
-
-      <h2 justify="center" class="header-page-name">
-        Position
-      </h2>
-      <v-spacer></v-spacer>
-
-      <!-- <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon> -->
-      <app-hamburger-menu></app-hamburger-menu>
-    </v-app-bar>
-    <v-footer
-      min-height="60px"
-      color="primaryblack"
-      absolute
-      class="font-weight-medium d-sm-none"
-    >
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
-      <router-link to="/emergency" class="icon-footer">
-        <v-img src="../../assets/alert-inactive.png" min-width="35" />
-      </router-link>
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
-      <router-link to="/position">
-        <v-img src="../../assets/explore-active.png" min-width="35" />
-      </router-link>
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
-      <router-link to="/quarantine-pass">
-        <v-img src="../../assets/key-inactive.png" min-width="35" />
-      </router-link>
-      <v-spacer></v-spacer>
+      </v-container>
+    </v-card>
+    <!--   ##################### -->
+    <!--  ###### FOOTER ###### -->
+    <!-- ################### -->
+    <v-footer color="primaryblack" absolute min-height="40px">
+      <v-container
+        class="font-weight-medium d-flex justify-space-between d-sm-none pa-0 ma-0"
+      >
+        <router-link to="/emergency" class="icon-footer margin-icon-left">
+          <v-img src="../../assets/alert-inactive.png" min-width="35%" />
+        </router-link>
+        <router-link to="/position" class="icon-footer margin-icon">
+          <v-img src="../../assets/explore-inactive.png" min-width="35%" />
+        </router-link>
+        <router-link
+          to="/quarantine-pass"
+          class="icon-footer margin-icon-right"
+        >
+          <v-img src="../../assets/key-inactive.png" min-width="35%" />
+        </router-link>
+      </v-container>
     </v-footer>
   </div>
 </template>
-
 <script>
-export default {};
+export default {
+  
+}
 </script>
+<style lang="scss" scoped>
+.margin-icon-left {
+  margin-left: 10%;
+}
 
-<style lang="scss" scoped></style>
+.margin-icon-right {
+  margin-right: 10%;
+}
+
+@media screen and (max-width: 768px) {
+  .box-shadow-none {
+    box-shadow: none !important;
+    border-radius: 0px;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .size {
+    max-width: 40vw;
+    min-height: 80vh;
+    margin: 0 auto;
+    margin-bottom: 5%;
+  }
+
+  #desktop-background {
+    background-image: url(../../assets/desktop-background-2.png);
+    background-size: auto 126%;
+    height: 100%;
+    width: 100vw;
+    background-position: center;
+    padding-top: 2%;
+  }
+}
+
+@media screen and (min-width: 1263px) {
+  .margin-right-fix {
+    margin-right: 7%;
+  }
+}
+</style>
