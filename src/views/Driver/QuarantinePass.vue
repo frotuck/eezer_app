1be2edfd66df4fae7d88762e2182cec828066d1a
jenseneducation-fredrik-blomberglarsson <fredrik.blomberg.larsson@gmail.com>
2020-11-09 14:26:45 +0100

    Added quarantine pass mobile & desktop view

--- src/views/Driver/QuarantinePass.vue
+++ src/views/Driver/QuarantinePass.vue
@@ -1,9 +1,144 @@
 <template>
-  <div>
-    <!-- ###################### -->
-    <!-- #### DESKTOP VIEW #### -->
-    <!-- ###################### -->
-    <div class="d-none d-sm-flex">
+  <div id="desktop-background">
+    <!--   ##################### -->
+    <!--  ###### HEADER ###### -->
+    <!-- ################### -->
+    <v-app-bar app color="primaryblack text-center pa-0">
+      <v-container
+        max-width="60%"
+        height="100%"
+        class="d-flex justify-space-around align-center pa-0 ma-0"
+      >
+        <h2
+          justify="center"
+          class="header-page-name justify-self-center d-md-none"
+        >
+          Quarantine Pass
+        </h2>
+        <v-container
+          max-width="60%"
+          height="100%"
+          class="d-none d-md-flex justify-space-around pa-0 ma-0"
+        >
+          <v-img
+            class="d-flex header-icon"
+            alt="Eezerlogo"
+            style="margin-left: 5%;"
+            src="../../assets/eezer-logo.png"
+            max-width="55"
+          />
+          <v-container
+            height="100%"
+            max-width="80%"
+            class="d-flex justify-end align-end pa-0 margin-right-fix"
+          >
+            <router-link to="/position">
+              <v-img
+                style="margin-bottom: -4px;"
+                alt="logout"
+                src="../../assets/map-desktop-inactive.png"
+                max-width="120px"
+              />
+            </router-link>
+            <router-link to="/profile">
+              <v-img
+                style="margin-bottom: -4px;"
+                alt="logout"
+                src="../../assets/profile-desktop-inactive.png"
+                max-width="120px"
+              />
+            </router-link>
+            <router-link to="/emergency">
+              <v-img
+                style="margin-bottom: -4px;"
+                alt="logout"
+                src="../../assets/emergency-desktop-inactive.png"
+                max-width="120px"
+              />
+            </router-link>
+          </v-container>
+        </v-container>
+      </v-container>
+      <app-hamburger-menu></app-hamburger-menu>
+    </v-app-bar>
+    <!--   ############################ -->
+    <!--  ###### PAGE CONTENT ####### -->
+    <!-- ########################## -->
+    <v-card class="box-shadow-none size">
+      <v-toolbar
+        class="primaryblack text-center d-none d-md-flex justify-center"
+        dark
+      >
+        <h3>Quarantine Pass</h3>
+      </v-toolbar>
+      <v-container class="d-flex flex-column">
+        <v-img
+          class="quarantineImage"
+          alt="quarantine-image"
+          src="../../assets/quarantine-image.svg"
+          max-width="400px"
+        />
+        <h3 class="qrHeader d-none d-md-flex">
+          Quarantine Pass ID
+        </h3>
+        <p class="qrInfo d-none d-md-flex">
+          Scan the QR Code to <br />
+          find information <br />
+          regarding the active <br />
+          Quarantine Pass. if <br />
+          failure to <br />
+          succuessfully scan <br />
+          the QR code, please <br />
+          manually fill in the <br />
+          PASS ID listed below <br />
+          the QR code.
+        </p>
+        <v-card class="accent passCard">
+          <h6>Pass ID</h6>
+          <v-icon class="editPencil">mdi-lead-pencil</v-icon>
+          <p class="passNum">XXXX-XXXX-XXXX-1908</p>
+        </v-card>
+        <router-link to="/faq" class="helpBtn d-none d-md-flex">
+          <v-btn
+            class="helpBtn justify-start pa-3 info"
+            depressed
+            elevation="2"
+            large
+            outlined
+            color="primaryblack"
+          >
+            <v-icon class="helpIcon pa-2">mdi-help-circle</v-icon>Help
+            <p class="btnParagraph">Questions?</p></v-btn
+          >
+        </router-link>
+      </v-container>
+    </v-card>
+    <!--   ##################### -->
+    <!--  ###### FOOTER ###### -->
+    <!-- ################### -->
+    <v-footer color="primaryblack" absolute min-height="40px">
+      <v-container
+        class="font-weight-medium d-flex justify-space-between d-sm-none pa-0 ma-0"
+      >
+        <router-link to="/emergency" class="icon-footer margin-icon-left">
+          <v-img src="../../assets/alert-inactive.png" min-width="35%" />
+        </router-link>
+        <router-link to="/position" class="icon-footer margin-icon">
+          <v-img src="../../assets/explore-inactive.png" min-width="35%" />
+        </router-link>
+        <router-link
+          to="/quarantine-pass"
+          class="icon-footer margin-icon-right"
+        >
+          <v-img src="../../assets/key-inactive.png" min-width="35%" />
+        </router-link>
+      </v-container>
+    </v-footer>
+  </div>
+  <!-- ###################### -->
+  <!-- #### DESKTOP VIEW #### -->
+  <!-- ###################### -->
+  <!--     <div class="d-none d-sm-flex">
       <v-app-bar app color="#202020">
         <v-img
           class="d-none d-sm-flex header-icon"
@@ -54,35 +189,64 @@
       absolute
       class="d-none d-sm-flex"
     >
-    </v-footer>
+    </v-footer> -->
 
-    <!-- ##################### -->
-    <!-- #### MOBILE VIEW #### -->
-    <!-- ##################### -->
-    <v-app-bar app class="d-sm-none" color="primaryblack">
+  <!-- ##################### -->
+  <!-- #### PAGE VIEW #### -->
+  <!-- ##################### -->
+  <!--     <v-app-bar app class="d-sm-none" color="primaryblack">
       <v-spacer></v-spacer>
 
       <h2 justify="center" class="header-page-name">
         Quarantine Pass
       </h2>
       <v-spacer></v-spacer>
-
-      <!-- <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon> -->
-      <app-hamburger-menu></app-hamburger-menu>
+ -->
+  <!-- <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon> -->
+  <!--       <app-hamburger-menu></app-hamburger-menu>
     </v-app-bar>
-    <v-container class="d-flex flex-column">
-      <v-img
-        class="quarantinePass"
-        alt="quarantine-pass"
-        src="../../assets/quarantine-image.svg"
-        max-width="100%"
-      />
-      <v-card class="accent passCard">
-        <h6>Pass ID</h6>
-        <v-icon class="editPencil">mdi-lead-pencil</v-icon>
-        <p class="passNum">XXXX-XXXX-XXXX-1908</p>
-        <p></p
-      ></v-card>
+    <v-container class="d-flex flex-column container">
+      <v-card>
+        <v-img
+          class="quarantineImage"
+          alt="quarantine-image"
+          src="../../assets/quarantine-image.svg"
+          max-width="400px"
+        />
+        <h3 class="qrHeader d-none d-sm-flex">
+          Quarantine Pass ID
+        </h3>
+        <p class="qrInfo d-none d-sm-flex">
+          Scan the QR Code to <br />
+          find information <br />
+          regarding the active <br />
+          Quarantine Pass. if <br />
+          failure to <br />
+          succuessfully scan <br />
+          the QR code, please <br />
+          manually fill in the <br />
+          PASS ID listed next <br />
+          to the QR code.
+        </p>
+        <v-card class="accent passCard">
+          <h6>Pass ID</h6>
+          <v-icon class="editPencil">mdi-lead-pencil</v-icon>
+          <p class="passNum">XXXX-XXXX-XXXX-1908</p>
+        </v-card>
+        <router-link to="/faq" class="helpBtn d-none d-sm-flex">
+          <v-btn
+            class="helpBtn justify-start pa-3 info"
+            depressed
+            elevation="2"
+            large
+            outlined
+            color="primaryblack"
+          >
+            <v-icon class="helpIcon pa-2">mdi-help-circle</v-icon>Help
+            <p class="btnParagraph">Questions?</p></v-btn
+          >
+        </router-link>
+      </v-card>
     </v-container>
     <v-footer
       min-height="60px"
@@ -106,8 +270,7 @@
         <v-img src="../../assets/key-active.png" min-width="35" />
       </router-link>
       <v-spacer></v-spacer>
-    </v-footer>
-  </div>
+    </v-footer> -->
 </template>
 
 <script>
@@ -115,7 +278,110 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.quarantinePass {
+@media screen and (min-width: 1263px) {
+  .margin-right-fix {
+    margin-right: 7%;
+  }
+}
+
+.margin-icon-left {
+  margin-left: 10%;
+}
+
+.margin-icon-right {
+  margin-right: 10%;
+}
+
+@media screen and (max-width: 768px) {
+  .box-shadow-none {
+    box-shadow: none !important;
+    border-radius: 0px;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .size {
+    max-width: 40vw;
+    min-height: 80vh;
+    margin: 0 auto;
+    margin-bottom: 5%;
+  }
+
+  #desktop-background {
+    background-color: #888c8f;
+    background-size: auto 126%;
+    height: 100%;
+    width: 100vw;
+    padding-top: 2%;
+  }
+  .editPencil {
+    display: none;
+  }
+  .quarantineImage {
+    width: 50%;
+  }
+
+  .qrHeader {
+    font-size: 1.1vw;
+    margin-left: 3%;
+    position: absolute;
+    top: 13%;
+  }
+
+  .qrInfo {
+    font-size: 1vw;
+    margin-left: 3%;
+    position: absolute;
+    top: 18%;
+  }
+}
+
+.passCard {
+  width: 80%;
+  height: 10%;
+  margin-left: 10%;
+  margin-top: 15%;
+}
+
+.editPencil {
+  position: absolute;
+  right: 5%;
+  top: 10%;
+  color: #f87502;
+}
+
+h6 {
+  margin-top: 6%;
+  margin-left: 5%;
+}
+
+.passNum {
+  margin-left: 5%;
+}
+
+.quarantineImage {
+  align-self: center;
+}
+
+.helpBtn {
+  width: 300px;
+  margin-bottom: 5%;
+  text-decoration: none;
+  align-self: center;
+  margin-top: 10%;
+}
+
+.introductionSubtext {
+  margin-bottom: -8%;
+}
+
+.btnParagraph {
+  position: absolute;
+  left: 65%;
+  color: #f9f9f9;
+}
+
+/* .quarantinePass {
   margin-top: 10%;
 }
 
@@ -142,4 +408,82 @@ h6 {
   margin-left: 5%;
   margin-top: 4%;
 }
+
+.quarantineImage {
+  align-self: center;
+}
+
+.helpBtn {
+  width: 300px;
+  margin-bottom: 5%;
+  text-decoration: none;
+  align-self: center;
+  margin-top: 10%;
+}
+
+.introductionSubtext {
+  margin-bottom: -8%;
+}
+
+.btnParagraph {
+  position: absolute;
+  left: 65%;
+  color: #f9f9f9;
+}
+
+@media screen and (min-width: 450px) {
+  .quarantineImage {
+    width: 80%;
+  }
+  .qrHeader {
+    display: none;
+  }
+  .qrInfo {
+    display: none;
+  }
+}
+
+@media screen and (min-width: 600px) {
+  .quarantineImage {
+    width: 50%;
+  }
+  .passCard {
+    position: absolute;
+    right: 5%;
+    bottom: 77%;
+    height: 15%;
+    width: 18%;
+  }
+
+  .qrHeader {
+    position: absolute;
+    top: 8%;
+  }
+
+  .qrInfo {
+    position: absolute;
+    top: 15%;
+  }
+
+  .editPencil {
+    display: none;
+  }
+}
+
+@media screen and (min-width: 1440px) {
+  .quarantineImage {
+    width: 50%;
+  }
+  .passCard {
+    position: absolute;
+    right: 10%;
+    bottom: 80%;
+    height: 12%;
+    width: 18%;
+  }
+
+  .editPencil {
+    display: none;
+  }
+} */
 </style>
