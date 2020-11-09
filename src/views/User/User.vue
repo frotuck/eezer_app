a8e36e9ca2244f65ca68fa8d4bf8abc733051be0
Artur <Artur.Hoppner@hotmail.com>
2020-11-09 15:18:37 +0100

    user driver-eta desktop and mobile done

--- src/views/User/User.vue
+++ src/views/User/User.vue
@@ -1,127 +1,219 @@
 <template>
-  <div>
-    <!-- ###################### -->
-    <!-- #### DESKTOP VIEW #### -->
-    <!-- ###################### -->
-    <div class="d-none d-sm-flex">
-      <v-app-bar app color="#202020">
-        <v-img
-          class="d-none d-sm-flex header-icon"
-          alt="Eezerlogo"
-          src="../../assets/eezer-logo.png"
-          max-width="55"
-        />
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <h2 justify="center" style="color" class="header-page-name">
-          Driver ETA
+  <div id="desktop-background">
+    <!--   ##################### -->
+    <!--  ###### HEADER ###### -->
+    <!-- ################### -->
+    <v-app-bar app color="primaryblack text-center pa-0">
+      <v-container
+        width="100%"
+        height="100%"
+        class="d-flex justify-space-around align-center pa-0 ma-0"
+      >
+        <h2
+          justify="center"
+          class="header-page-name justify-self-center d-md-none"
+        >
+          Profile
         </h2>
-        <v-spacer></v-spacer>
-        <router-link to="/user">
+        <v-container
+          width="100%"
+          height="100%"
+          class="d-none d-md-flex justify-space-around pa-0 ma-0"
+        >
           <v-img
-            style="margin-bottom: -26px"
-            alt="logout"
-            src="../../assets/map-desktop-active.png"
-            max-width="120px"
+            class="d-flex header-icon"
+            alt="Eezerlogo"
+            style="margin-left: 5%;"
+            src="../../assets/eezer-logo.png"
+            max-width="55"
           />
-        </router-link>
-        <router-link to="/userprofile">
-          <v-img
-            style="margin-bottom: -26px"
-            alt="logout"
-            src="../../assets/profile-desktop-inactive.png"
-            max-width="120px"
-          />
-        </router-link>
-        <router-link to="/useremergency">
-          <v-img
-            style="margin-bottom: -26px"
-            alt="logout"
-            src="../../assets/emergency-desktop-inactive.png"
-            max-width="120px"
-          />
-        </router-link>
-        <router-link to="/">
-          <v-img
-            style="margin-bottom: -26px; margin-left: 10px;"
-            alt="logout"
-            src="../../assets/logout-icon.png"
-            max-width="30"
-          />
-        </router-link>
-        <h4 style=" margin-bottom: -26px; color: white">
-          Logout
-        </h4>
-      </v-app-bar>
-    </div>
-    <v-footer
-      min-height="40px"
-      color="#202020"
-      absolute
-      class="d-none d-sm-flex"
-    >
-    </v-footer>
-    <!-- ##################### -->
-    <!-- #### MOBILE VIEW #### -->
-    <!-- ##################### -->
-    <v-app-bar app class="d-sm-none" color="primaryblack">
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
-
-      <h2 justify="center" style="color" class="header-page-name">
-        Driver ETA
-      </h2>
-      <v-spacer></v-spacer>
+          <v-container
+            height="100%"
+            class="d-flex justify-end align-end pa-0 margin-right-fix"
+          >
+            <router-link to="/user">
+              <v-img
+                style="margin-bottom: -4px;"
+                alt="map"
+                src="../../assets/map-desktop-active.png"
+                max-width="120px"
+              />
+            </router-link>
+            <router-link to="/userprofile">
+              <v-img
+                style="margin-bottom: -4px;"
+                alt="logout"
+                src="../../assets/profile-desktop-inactive.png"
+                max-width="120px"
+              />
+            </router-link>
+            <router-link to="/useremergency">
+              <v-img
+                style="margin-bottom: -4px;"
+                alt="logout"
+                src="../../assets/emergency-desktop-inactive.png"
+                max-width="120px"
+              />
+            </router-link>
+          </v-container>
+        </v-container>
+      </v-container>
       <router-link to="/">
-        <v-img alt="logout" src="../../assets/logout-icon.png" max-width="35" />
+        <v-img alt="logout" src="../../assets/logout-icon.png" max-width="30" />
       </router-link>
     </v-app-bar>
-
-    <v-img class="d-sm-none" src="../../assets/map-mobile-large.png"></v-img>
-    <div class="d-sm-none">
-      <div>
-        <h4>To Sesame Street</h4>
-
+    <!--   ############################ -->
+    <!--  ###### PAGE CONTENT ####### -->
+    <!-- ########################## -->
+    <v-card
+      class="mx-auto mt-sm-15 mt-8"
+      max-width="800"
+      style=" margin-bottom: 50px"
+    >
+      <v-toolbar
+        class="primaryblack text-center d-none d-md-flex justify-center"
+        dark
+      >
+        <h3>Route</h3>
+      </v-toolbar>
+      <v-img
+        class="white--text align-end"
+        height="500px"
+        src="../../assets/map-desktop-large.png"
+      >
+      </v-img>
+      <div class="d-flex space-around">
         <div>
-          <p>Time</p>
-          <h4>25 min</h4>
+          <v-card-subtitle class="pb-0" color="">
+            To Sesame Street
+          </v-card-subtitle>
+          <div class="d-flex ">
+            <div>
+              <v-card-subtitle class="pb-0">
+                Time
+              </v-card-subtitle>
+              <v-card-text>25min</v-card-text>
+            </div>
+            <div>
+              <v-card-subtitle class="pb-0">
+                Distance
+              </v-card-subtitle>
+              <v-card-text>20km</v-card-text>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>Distance</p>
-          <h4>9. mi</h4>
+        <div class="d-flex flex-column">
+          <v-card-subtitle class="pb-0">
+            Driver Info
+          </v-card-subtitle>
+          <v-card-actions>
+            <app-dialog-patient></app-dialog-patient>
+          </v-card-actions>
         </div>
       </div>
-      <v-img src="../../assets/information-icon.png" max-width="35" />
-    </div>
-
-    <v-footer
-      min-height="60px"
-      color="primaryblack"
-      absolute
-      class="font-weight-medium d-sm-none"
-    >
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
-      <router-link to="/useremergency" class="icon-footer">
-        <v-img src="../../assets/alert-inactive.png" min-width="35" />
-      </router-link>
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
-      <router-link to="/user">
-        <v-img src="../../assets/explore-active.png" min-width="35" />
-      </router-link>
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
-      <router-link to="/userprofile">
-        <v-img src="../../assets/profile-inactive.png" min-width="35" />
-      </router-link>
-      <v-spacer></v-spacer>
+    </v-card>
+    <!--   ##################### -->
+    <!--  ###### FOOTER ###### -->
+    <!-- ################### -->
+    <v-footer color="primaryblack" absolute min-height="40px">
+      <v-container
+        class="font-weight-medium d-flex d-sm-none d-none d-sm-flex d-md-none justify-space-between pa-0 ma-0"
+      >
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <router-link to="/useremergency" class="icon-footer">
+          <v-img src="../../assets/alert-inactive.png" min-width="35" />
+        </router-link>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <router-link to="/user">
+          <v-img src="../../assets/explore-active.png" min-width="35" />
+        </router-link>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <router-link to="/userprofile">
+          <v-img src="../../assets/profile-inactive.png" min-width="35" />
+        </router-link>
+        <v-spacer></v-spacer>
+      </v-container>
     </v-footer>
   </div>
 </template>
-
 <script>
-export default {};
+export default {
+  name: 'UserProfile',
+};
 </script>
+<style lang="scss" scoped>
+.boxshadow {
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.158);
+}
 
-<style lang="scss" scoped></style>
+.margin-bot {
+  margin-bottom: 15%;
+}
+
+.margin-left {
+  margin-left: 2%;
+}
+
+@media screen and (max-width: 768px) {
+  .box-shadow-none {
+    box-shadow: none !important;
+  }
+}
+@media screen and (max-width: 920px) {
+  .space-around {
+    display: flex;
+    justify-content: center;
+  }
+}
+
+@media screen and (min-width: 0px) {
+  .responsive {
+    max-width: 80%;
+    max-height: 80%;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .size {
+    max-width: 40vw;
+    min-height: 80vh;
+    margin: 0 auto;
+    margin-bottom: 5%;
+  }
+
+  .responsive {
+    max-width: 90%;
+    max-height: 90%;
+    padding-bottom: 5%;
+  }
+
+  #desktop-background {
+    background-image: url(../../assets/desktop-background-2.png);
+    background-size: auto 126%;
+    height: 100%;
+    width: 100vw;
+    background-position: center;
+    padding-top: 2%;
+  }
+}
+
+@media screen and (min-width: 1263px) {
+  .margin-right-fix {
+    margin-right: 7%;
+  }
+}
+
+@media screen and (min-width: 1440px) {
+  .size {
+    max-width: 40vw;
+    min-height: 80vh;
+  }
+
+  .responsive {
+    padding-bottom: 10%;
+  }
+}
+</style>
--- src/views/User/UserEmergency.vue
+++ src/views/User/UserEmergency.vue
@@ -13,7 +13,7 @@
           justify="center"
           class="header-page-name justify-self-center d-md-none"
         >
-          Emergency
+          Profile
         </h2>
         <v-container
           width="100%"
@@ -43,7 +43,7 @@
               <v-img
                 style="margin-bottom: -4px;"
                 alt="logout"
-                src="../../assets/profile-desktop-inactive.png"
+                src="../../assets/profile-desktop-active.png"
                 max-width="120px"
               />
             </router-link>
@@ -51,7 +51,7 @@
               <v-img
                 style="margin-bottom: -4px;"
                 alt="logout"
-                src="../../assets/emergency-desktop-active.png"
+                src="../../assets/emergency-desktop-inactive.png"
                 max-width="120px"
               />
             </router-link>
@@ -70,27 +70,34 @@
         class="primaryblack text-center d-none d-md-flex justify-center"
         dark
       >
-        <h3>Emergency</h3>
+        <h3>My profile</h3>
       </v-toolbar>
+      <v-container class="boxshadow pa-0">
+        <v-img src="@/assets/profile-picture.png"></v-img>
+      </v-container>
+      <v-container>
+        <h3>J Doe</h3>
+        <h6>San Francisco, CA</h6>
+        <p>
+          J i planned to give birth 4 of january 2021. She has no known illnes
+          that could affect the transportation to a hospital.
+        </p>
+      </v-container>
+      <v-container class="  d-flex flex-column 	margin-bot responsive">
+        <div class="  d-flex flex-row align-left ">
+          <v-img
+            alt="logout"
+            src="../../assets/location-icon.png"
+            max-width="20px"
+            max-height="23px"
+          />
 
-      <v-container class="  d-flex flex-column  	margin-bot responsive">
-        <h3 class="center-text">What to do</h3>
-        <p>
-          Click on Request Emergency Help to get in contact with 911 or customer
-          service for immediate help.
-        </p>
-        <p>
-          CPR: Call 911 or ask someone else to. Lay the person on their back and
-          open their airway. Check for breathing. If they are not breathing,
-          start CPR. Perform 30 chest compressions. Perform two rescue breaths.
-          Repeat until an ambulance or automated external defibrillator (AED)
-          arrives.
-        </p>
-        <v-img
-          alt="logout"
-          src="../../assets/warning-mobile.png"
-          max-width="80vw"
-        />
+          <p>My Location</p>
+        </div>
+
+        <div class="  d-flex flex-column align-right ">
+          <v-img alt="logout" src="../../assets/map-small.png" />
+        </div>
       </v-container>
     </v-card>
     <!--   ##################### -->
@@ -103,7 +110,7 @@
         <v-spacer></v-spacer>
         <v-spacer></v-spacer>
         <router-link to="/useremergency" class="icon-footer">
-          <v-img src="../../assets/alert-active.png" min-width="35" />
+          <v-img src="../../assets/alert-inactive.png" min-width="35" />
         </router-link>
         <v-spacer></v-spacer>
         <v-spacer></v-spacer>
@@ -113,7 +120,7 @@
         <v-spacer></v-spacer>
         <v-spacer></v-spacer>
         <router-link to="/userprofile">
-          <v-img src="../../assets/profile-inactive.png" min-width="35" />
+          <v-img src="../../assets/profile-active.png" min-width="35" />
         </router-link>
         <v-spacer></v-spacer>
       </v-container>
@@ -122,13 +129,10 @@
 </template>
 <script>
 export default {
-  name: 'UserEmergency',
+  name: 'UserProfile',
 };
 </script>
 <style lang="scss" scoped>
-.center-text {
-  text-align: center;
-}
 .boxshadow {
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.158);
 }
